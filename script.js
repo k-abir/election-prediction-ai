00d0c9f255519f651d40d99c083b3e3992334e6c
kabir <143556475+k-abir@users.noreply.github.com>
2024-06-07 10:09:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const socialMediaSelect = document.getElementById('social-media');
-    const generateResultsButton = document.getElementById('generate-results');
-    const resultsContainer = document.getElementById('results-container');
+// Generate preset data based on social media selection
+function generatePresetData() {
+    const socialMedia = document.getElementById('socialMedia').value;
+    let data;
+    switch(socialMedia) {
+        case 'reddit':
+            data = "Reddit data preset: More Democratic";
+            break;
+        case 'twitter':
+            data = "Twitter data preset: Mixed";
+            break;
+        case 'facebook':
+            data = "Facebook data preset: More Republican";
+            break;
+    }
+    alert(data);
+}
 
-    generateResultsButton.addEventListener('click', function() {
-        const selectedPlatform = socialMediaSelect.value;
-        let results;
+// Create a US map using D3.js
+const width = 960;
+const height = 600;
 
-        switch (selectedPlatform) {
-            case 'reddit':
-                results = "Results: Predominantly Democratic";
-                break;
-            case 'twitter':
-                results = "Results: Mixed";
-                break;
-            case 'facebook':
-                results = "Results: Predominantly Republican";
-                break;
-            default:
-                results = "No data available";
-        }
+const svg = d3.select("#map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-        resultsContainer.textContent = results;
-    });
+const projection = d3.geoAlbersUsa()
+    .scale(1000)
+    .translate([width / 2, height / 2]);
 
-    // D3.js for drawing the US map
-    const width = 960;
-    const height = 600;
+const path = d3.geoPath()
+    .projection(projection);
 
-    const svg = d3.select("#us-map").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+d3.json("https://d3js.org/us-10m.v1.json").then(us => {
+    svg.append("g")
+        .attr("class", "states")
+      .selectAll("path")
+      .data(topojson.feature(us, us.objects.states).features)
+      .enter().append("path")
+        .attr("d", path);
 
-    const projection = d3.geoAlbersUsa()
-        .scale(1280)
-        .translate([width / 2, height / 2]);
-
-    const path = d3.geoPath()
-        .projection(projection);
-
-    d3.json("https://d3js.org/us-10m.v1.json").then(function(us) {
-        svg.append("g")
-            .attr("class", "counties")
-            .selectAll("path")
-            .data(topojson.feature(us, us.objects.counties).features)
-            .enter().append("path")
-            .attr("d", path);
-
-        svg.append("path")
-            .attr("class", "county-borders")
-            .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
-    });
+    svg.append("path")
+        .attr("class", "state-borders")
+        .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
 });
