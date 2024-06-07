bf7bc6485054df85aa14d23d78520884787a6fde
kabir <143556475+k-abir@users.noreply.github.com>
2024-06-07 10:33:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
 
-from flask import Flask, render_template
-from us_map import create_us_map
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/us_map')
-def us_map():
-    img = create_us_map()
-    return send_file(img, mimetype='image/png')
-
-if __name__ == '__main__':
-    app.run(debug=True)
